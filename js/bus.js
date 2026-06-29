b4e4075be80bd86afab432ce9d4d8e54874e0e04
karlokwong <166092445+karlokwong@users.noreply.github.com>
2026-06-29 17:57:09 +0800

    Update bus.js

--- js/bus.js
+++ js/bus.js
@@ -8,9 +8,9 @@
 
 /* ══ PRESET STOPS ═══════════════════════════════════════════ */
 const PRESET_STOPS = [
-  { operator:'KMB', id:'61D7306AC40C4FB8', label:'青衣碼頭總站', route:'41A', serviceType:1, hint:'往旺角' },
+  { operator:'KMB', id:'61D7306AC40C4FB8', label:'青衣碼頭總站', route:'41A', serviceType:1, hint:'往旺角/尖沙咀東' },
   { operator:'KMB', id:'61D7306AC40C4FB8', label:'青衣碼頭總站', route:'44', serviceType:1, hint:'往旺角' },
-  { operator:'KMB', id:'45C39BB56C6B333C', label:'大興總站',       route:'66M', serviceType:1, hint:'往元朗' },
+  { operator:'KMB', id:'1FEDF7B1A82A432A', label:'青衣碼頭總站', route:'43B', serviceType:1, hint:'往荃灣' },
   { operator:'KMB', id:'507A1E5DF62D2B4A', label:'天瑞總站',       route:'69X', serviceType:1, hint:'往沙田' },
   { operator:'CTB', id:'001939',           label:'龍門居',         route:'962X', hint:'往銅鑼灣' },
 ];
